15eb976b1a80db47a718885db28a4e2d1fcf375f
Glitch (glitch-hello-website) <none>
2023-04-13 06:39:50 +0000

    👴🚉 Checkpoint ./games.html:41112806/4167 ./script.js:41112806/664

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
-
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
-    btn.classList.toggle("dipped");
-  };
-}
 
 
 // ----- GLITCH STARTER PROJECT HELPER CODE -----
